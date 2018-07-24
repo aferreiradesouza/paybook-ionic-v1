5a9e8f27d9e565edfcb3bc16a094c052395fc354
Arthur <arthur.csm2@gmail.com>
2018-07-23 22:59:55 -0300

    ajuste

--- www/js/app.js
+++ www/js/app.js
@@ -4,12 +4,13 @@ var app = angular.module('starter', ['ionic', 'starter.controllers', 'starter.se
       if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
         cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         cordova.plugins.Keyboard.disableScroll(true);
-        cordova.plugins.Keyboard.shrinkView(true, () => {
-          alert('foi cord');
-        });
-        Keyboard.shrinkView(true, () => {
-          alert('foi');
-        });
+        // alert('foi fora');
+        // cordova.plugins.Keyboard.shrinkView(true, () => {
+        //   alert('foi cord');
+        // });
+        // Keyboard.shrinkView(true, () => {
+        //   alert('foi');
+        // });
       }
       if (window.StatusBar) {
         // org.apache.cordova.statusbar required
